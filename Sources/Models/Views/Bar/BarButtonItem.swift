e7949893002b57da30dfecbf2dc9e5d6c0d19e87
Md. Ibrahim Hassan <mdibrahimhassan@gmail.com>
2021-08-11 18:17:47 +0530

    Refactored Bar Button Code

--- Sources/Models/Views/Bar/BarButtonItem.swift
+++ Sources/Models/Views/Bar/BarButtonItem.swift
@@ -11,27 +11,20 @@ public struct BarButtonItem: IBDecodable {
     public var style: String?
     public var systemItem: String?
     public var title: String?
-    public var imageReferences: [ImageReference]?
+    public var imageReference: ImageReference?
     public var image: String?
     public var landscapeImage: String?
 
     static func decode(_ xml: XMLIndexerType) throws -> BarButtonItem {
-        let container = xml.container(keys: MappedCodingKey.self).map { (key: CodingKeys) in
-            let stringValue: String = {
-                switch key {
-                case .imageReferences: return "imageReference"
-                default: return key.stringValue
-                }
-            }()
-            return MappedCodingKey(stringValue: stringValue)
-        }
+        let container = xml.container(keys: CodingKeys.self)
+        
         return BarButtonItem(
             id:             try container.attribute(of: .id),
             key:                container.attributeIfPresent(of: .key),
             style:              container.attributeIfPresent(of: .style),
             systemItem:         container.attributeIfPresent(of: .systemItem),
             title:              container.attributeIfPresent(of: .title),
-            imageReferences:    container.elementsIfPresent(of: .imageReferences),
+            imageReference:    container.elementIfPresent(of: .imageReference),
             image:              container.attributeIfPresent(of: .image),
             landscapeImage:     container.attributeIfPresent(of: .landscapeImage)
         )
