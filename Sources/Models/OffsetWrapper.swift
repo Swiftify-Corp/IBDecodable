fe933f36fc0c2f24c649412a5082c2ec0f26affd
Md Ibrahim Hassan <mdibrahimhassan@gmail.com>
2021-09-11 21:37:46 +0530

    Made Float values inside OffsetWrapper non optional

--- Sources/Models/OffsetWrapper.swift
+++ Sources/Models/OffsetWrapper.swift
@@ -9,15 +9,15 @@
 
 public struct OffsetWrapper: IBDecodable, IBKeyable {
     public var key: String?
-    public var horizontal: Float?
-    public var vertical: Float?
+    public var horizontal: Float
+    public var vertical: Float
 
     static func decode(_ xml: XMLIndexerType) throws -> OffsetWrapper {
         let container = xml.container(keys: CodingKeys.self)
         return OffsetWrapper(
             key: container.attributeIfPresent(of: .key),
-            horizontal: container.attributeIfPresent(of: .horizontal),
-            vertical: container.attributeIfPresent(of: .vertical)
+            horizontal: container.attribute(of: .horizontal),
+            vertical: container.attribute(of: .vertical)
         )
     }
 }
