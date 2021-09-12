2615a8e85e9a7b295d2e34e8fa7094afec923b6e
Md Ibrahim Hassan <mdibrahimhassan@gmail.com>
2021-09-12 22:16:32 +0530

    Added badge Color and value properties

--- Sources/Models/Views/Bar/TabBarItem.swift
+++ Sources/Models/Views/Bar/TabBarItem.swift
@@ -24,6 +24,11 @@ public struct TabBarItem: IBDecodable, IBKeyable, IBCustomClassable, IBUserLabel
     public var image: String?
     public var selectedImage: String?
     public var catalog: String?
+    public var badgeValue: String?
+    public var badgeColor: Color?
+    
+    enum ExternalCodingKeys: CodingKey { case color }
+    enum ColorsCodingKeys: CodingKey { case key }
 
     static func decode(_ xml: XMLIndexerType) throws -> TabBarItem {
         let container = xml.container(keys: MappedCodingKey.self).map { (key: CodingKeys) in
@@ -35,23 +40,27 @@ public struct TabBarItem: IBDecodable, IBKeyable, IBCustomClassable, IBUserLabel
             }()
             return MappedCodingKey(stringValue: stringValue)
         }
+        let colorsContainer = xml.container(keys: ExternalCodingKeys.self)
+            .nestedContainerIfPresent(of: .color, keys: ColorsCodingKeys.self)
 
         return TabBarItem(
-            id:               try container.attribute(of: .id),
-            key:                  container.attributeIfPresent(of: .key),
-            style:                container.attributeIfPresent(of: .style),
-            systemItem:           container.attributeIfPresent(of: .systemItem),
-            title:                container.attributeIfPresent(of: .title),
-            customClass:          container.attributeIfPresent(of: .customClass),
-            customModule:         container.attributeIfPresent(of: .customModule),
-            customModuleProvider: container.attributeIfPresent(of: .customModuleProvider),
-            userLabel:            container.attributeIfPresent(of: .userLabel),
-            colorLabel:           container.attributeIfPresent(of: .colorLabel),
-            imageReferences:      container.elementsIfPresent(of: .imageReferences),
-            offsetWrapper:        container.elementIfPresent(of: .offsetWrapper),
-            image:                container.attributeIfPresent(of: .image),
-            selectedImage:        container.attributeIfPresent(of: .selectedImage),
-            catalog:              container.attributeIfPresent(of: .catalog)
+            id:                         try container.attribute(of: .id),
+            key:                            container.attributeIfPresent(of: .key),
+            style:                          container.attributeIfPresent(of: .style),
+            systemItem:                     container.attributeIfPresent(of: .systemItem),
+            title:                          container.attributeIfPresent(of: .title),
+            customClass:                    container.attributeIfPresent(of: .customClass),
+            customModule:                   container.attributeIfPresent(of: .customModule),
+            customModuleProvider:           container.attributeIfPresent(of: .customModuleProvider),
+            userLabel:                      container.attributeIfPresent(of: .userLabel),
+            colorLabel:                     container.attributeIfPresent(of: .colorLabel),
+            imageReferences:                container.elementsIfPresent(of: .imageReferences),
+            offsetWrapper:                  container.elementIfPresent(of: .offsetWrapper),
+            image:                          container.attributeIfPresent(of: .image),
+            selectedImage:                  container.attributeIfPresent(of: .selectedImage),
+            catalog:                        container.attributeIfPresent(of: .catalog),
+            badgeValue:                     container.attributeIfPresent(of: .badgeValue),
+            badgeColor:                     colorsContainer?.withAttributeElement(.key, CodingKeys.badgeColor.stringValue)
         )
     }
 }
