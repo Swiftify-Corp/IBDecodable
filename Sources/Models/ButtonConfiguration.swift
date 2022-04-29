5ac4b941a9e6812000a6abcf35129ded570c7ef5
Md Ibrahim Hassan <mdibrahimhassan@gmail.com>
2022-04-29 16:08:22 +0530

    add string container to button

--- Sources/Models/ButtonConfiguration.swift
+++ Sources/Models/ButtonConfiguration.swift
@@ -9,13 +9,15 @@ public struct ButtonConfiguration: IBDecodable, IBKeyable {
     public let key: String?
     public let style: String?
     public let title: String?
+    public let string: StringContainer?
     
     static func decode(_ xml: XMLIndexerType) throws -> ButtonConfiguration {
         let container = xml.container(keys: CodingKeys.self)
         return ButtonConfiguration(
             key:    container.attributeIfPresent(of: .key),
             style:  container.attributeIfPresent(of: .style),
-            title: container.attributeIfPresent(of: .title)
+            title: container.attributeIfPresent(of: .title),
+            string: container.elementIfPresent(of: .string)
         )
     }
 }
